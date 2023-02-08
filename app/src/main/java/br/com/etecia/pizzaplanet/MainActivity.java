b64dded046afd318cce426451c277abf0289759d
Sheltex <shelton.exbol@hotmail.com>
2023-02-07 21:23:31 -0300

    adaptador

--- app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
+++ app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.pizzaplanet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            idListaValores = findViewById(R.id.idListaValores);
+            MyAdapter adapter=new MyAdapter();
+            idListaValores.setAdapter(adapter);
+
+    }
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
