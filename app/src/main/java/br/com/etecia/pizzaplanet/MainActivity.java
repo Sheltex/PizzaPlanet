0b7c5793eafe6296de31d3a504254cff6104b6a7
Sheltex <shelton.exbol@hotmail.com>
2023-02-14 22:02:13 -0300

    atualização3

--- app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
+++ app/src/main/java/br/com/etecia/pizzaplanet/MainActivity.java
@@ -2,14 +2,26 @@ package br.com.etecia.pizzaplanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView idListaValores;
+
+    String idTitulo [] = {"Pizza Toscana"};
+    String idDesc[] = {"Mais uma tradição da Itália, a Toscana da Rede Leve Pizza com muita calabresa ralada e bacon vai levar um sabor diferenciado para sua mesa. Experimente, é garantia de satisfação."};
+    String idNota[] = {"0.5"};
+    int idImagemPizza [] = {R.drawable.toscana};
+
+    double rating[] = {0.5};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return idImagemPizza.length;
         }
 
         @Override
@@ -42,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int i, View view, ViewGroup viewGroup) {
             return null;
         }
+
+       ImageView img;
+        TextView t, st, n;
+
+        View v = getLayoutInflater().inflate(R.layout.modelo_cardapio, null);
+        img = v.fi
+
+
+
     }
 }
